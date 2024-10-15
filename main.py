6894b603806f4d293f668c07c2664c1fad2b311d
ST4N3R <stasgozdzikowski@gmail.com>
2024-10-15 23:51:22 +0200

    Run first analysis in Spark

--- main.py
+++ main.py
@@ -1,28 +1,35 @@
 from flask import Flask, request, redirect
 from src.auth.auth import get_authorization_url, get_access_token
-from src.data.api_request import get_user_top_items
+from src.processing.api_request import get_user_top_items
+import json
 
 app = Flask(__name__)
 
+#Główna strona. Pierszą rzeczą, którą robi jest przekierowanie na stronę logowania Spotify i pobranie zgody na SCOPES. 
+#Po otrzymaniu zgód API Spotify przekierowuje stronę na /callback
+#TO DO: Dodać frontend
 @app.route('/')
 def home():
-    # Redirect user to Spotify login
     auth_url = get_authorization_url()
     return redirect(auth_url)
 
+#Najpierw pobiera odpowiednie tokeny, a nastepnie używa ich do pobrania danych z GET USER'S TOP ITEMS. Na końcu zwraca wyniki
+#TO DO: Następnie powinien przesyłać dane do Azure i obrabiać je
 @app.route('/callback/')
 def callback():
-    # Get authorization code from Spotify
     auth_code = request.args.get('code')
     
-    # Exchange authorization code for access token
     access_token = get_access_token(auth_code)
     
-    # Fetch user's top tracks
     top_tracks = get_user_top_items(access_token, item_type='tracks', time_range='long_term')
-    
-    # Display the results
-    return top_tracks  # You'll likely want to process or display this differently
+
+    items = top_tracks['items']
+    next_url = top_tracks['next']
+
+    with open("data/data.json", "w") as json_file:
+        json.dump(items, json_file, indent=4)
+
+    return top_tracks
 
 if __name__ == '__main__':
     app.run(debug=True, port=8888)
---
+++ src/processing/api_request.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def get_user_top_items(access_token, item_type='tracks', time_range='medium_term', limit=20):
+    """Gets the data from GET USE'S TOP ITEMS API
+
+    Args:
+        access_token (str): code that gives access to the API
+        item_type (str, optional): specifies what type of data we get - tracks or artists. Defaults to 'tracks'.
+        time_range (str, optional): specifies from what period we get the data. Defaults to 'medium_term'.
+        limit (int, optional): set the limit of reposnds, max 50. Defaults to 20.
+
+    Returns:
+        _type_: _description_
+    """
+
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    url = f"https://api.spotify.com/v1/me/top/{item_type}"
+    params = {
+        'time_range': time_range,
+        'limit': limit 
+    }
+    response = requests.get(url, headers=headers, params=params)
+    return response.json()
